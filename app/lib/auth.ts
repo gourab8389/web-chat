f5c0013f26d654216aea49751f751869207c45f9
gourab8389 <deyg6988@gmail.com>
2024-09-05 19:55:44 +0530

    feat: Add NextAuth authentication with GitHub provider
    This commit adds NextAuth authentication to the application using the GitHub provider. It includes the necessary code changes in the `route.ts` file and the `auth.ts` file. The `authOptions` object in the `auth.ts` file now includes the GitHub provider configuration with the client ID and client secret obtained from environment variables.

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,11 +1,18 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client/extension";
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+
+const prisma = new PrismaClient()
+
 export const authOptions : NextAuthOptions = {
+    // @ts-ignore
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
-            clientId: "",
-            clientSecret: ""
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_SECRET_ID as string,
         })
-    ]
+    ],
 }
