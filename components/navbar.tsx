10d13e17e2e86e1626c0de53066ff517bd11bde7
gourab8389 <deyg6988@gmail.com>
2024-09-06 18:31:46 +0530

    feat: Add lazy loading for user profile images

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,29 @@
+import { authOptions } from '@/app/lib/auth'
+import { getServerSession } from 'next-auth'
+import Image from 'next/image'
 import React from 'react'
+import { LoginButton, LogoutButton } from './ui/buttons'
 
-const Navbar = () => {
+const Navbar = async () => {
+
+    const session = await getServerSession(authOptions)
+
   return (
     <nav className="flex px-10 py-5 justify-between fixed top-0 left-0 w-full bg-white">
-        <h1 className="text-2xl font-bold">NextAuth.js</h1>
-        <div>
-            <a href="/" className="mx-2">Home</a>
-            <a href="/profile" className="mx-2">Profile</a>
-            <a href="/api/auth/signout" className="mx-2">Sign Out</a>
-        </div>
+        <h1 className='text-black text-3xl font-bold'>Trilu <span className='text-fuchsia-900'>Chat</span></h1>
+        {session ? 
+        <div className="flex items-center gap-5">
+            <Image 
+            src={session.user?.image as string} alt='user profile photo'
+            className='w-full h-full rounded-full mr-2'
+            height={20}
+            width={20}
+            />
+            <LogoutButton/>
+        </div> 
+        :
+        <LoginButton/>
+        }
     </nav>
   )
 }
---
+++ components/ui/buttons.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { signIn, signOut } from "next-auth/react"
+
+
+import { ButtonHTMLAttributes, FC } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+  size?: "small" | "medium" | "large";
+  variant?: "primary" | "secondary" | "danger";
+}
+
+export const Button: FC<ButtonProps> = ({ label, size = "medium", variant = "primary", ...props }) => {
+  const baseClasses = "rounded-md font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2";
+  
+  const sizeClasses = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+  
+  const variantClasses = {
+    primary: "bg-fuchsia-700 text-white hover:bg-fuchsia-800 focus:ring-blue-300",
+    secondary: "bg-fuchsia-500 text-white hover:bg-fuchsia-600 focus:ring-gray-300",
+    danger: "bg-red-500 text-white hover:bg-red-600 focus:ring-red-300",
+  };
+
+  return (
+    <button
+      className={`${baseClasses} ${sizeClasses[size]} ${variantClasses[variant]}`}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+
+
+
+
+
+
+export const LogoutButton = () => {
+  return (
+    <button onClick={() => signOut()} className="flex items-center justify-center gap-2 rounded-lg bg-red-500 px-8 py-3 text-center text-sm font-semibold text-white ring-red-300 transition duration-100 hover:bg-red-600 md:text-base">
+        Logout
+    </button>
+  )
+}
+
+
+export const LoginButton = () => {
+    return (
+        <button onClick={() => signIn()} className="flex items-center justify-center gap-2 rounded-lg bg-fuchsia-700 px-8 py-3 text-center text-sm font-semibold text-white ring-fuchsia-500 transition duration-100 hover:bg-fuchsia-900 md:text-base">
+            Login
+        </button>
+    )
+}
