6c4ef5b9b1c9847b53dd40db5d63c6225fd2c81c
gourab8389 <deyg6988@gmail.com>
2024-09-11 20:59:37 +0530

    feat: Add server-side action to post chat message
    This commit adds a new server-side action called `postData` to handle posting chat messages. The action is imported from the `action.ts` file and is used in the chat form component (`form.tsx`). When the form is submitted, the `postData` function is called with the form data to create a new message in the database using Prisma. The created message includes the message content and the email of the authenticated user. The `postData` function also includes a `User` field in the created message to select the user's name and image from the database.
    
    The commit also includes a new file `db.ts` in the `lib` directory, which exports a `prisma` instance for interacting with the database using Prisma. The `prisma` instance is created if it doesn't already exist and is made available globally. This file is used in the `postData` function to create the message in the database.
    
    Lastly, the `next.config.mjs` file is updated to enable the experimental feature `serverActions`. This feature allows the usage of server-side actions in Next.js.

--- app/chat/[components]/form.tsx
+++ app/chat/[components]/form.tsx
@@ -1,16 +1,21 @@
 "use client"
 
+import { postData } from "@/app/action";
+
 const Form = () => {
   return (
-    <form className="p-6 fixed bottom-0 left-0 w-full bg-white">
+    <form action={async (formData)=>{
+      await postData(formData)
+    }} className="p-6 fixed bottom-0 left-0 w-full bg-white">
         <div className="flex">
             <input 
             type="text" name="message" placeholder="Type your message..."
             className="flex-grow py-2 px-4 outline-none"
             />
+            <button className="flex items-center justify-center gap-2 rounded-full bg-fuchsia-700 px-8 py-3 text-center text-sm font-semibold text-white ring-fuchsia-500 transition duration-100 hover:bg-fuchsia-900 md:text-base" type="submit">Send</button>
         </div>
     </form>
   )
 }
 
-export default Form
+export default Form;
