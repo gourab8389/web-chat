6810aa42f99af64d43c653d61ac6cda7a243806e
gourab8389 <deyg6988@gmail.com>
2024-09-11 23:47:34 +0530

    feat: Integrate Pusher for real-time chat updates
    - Added Pusher library for real-time communication
    - Updated postData function to trigger Pusher event
    - Subscribed to Pusher channel in ChatComponent
    - Updated ChatComponent to display real-time chat messages
    - Added scroll-to-bottom functionality for chat messages

--- app/action.ts
+++ app/action.ts
@@ -4,8 +4,12 @@ import { getServerSession } from "next-auth";
 import { prisma } from "./lib/db";
 import { authOptions } from "./lib/auth";
 
+
+
 export async function postData(formData : FormData) {
     "use server";
+
+    const Pusher = require("pusher")
 
     const session = await getServerSession(authOptions)
     const message = formData.get("message")
@@ -23,5 +27,15 @@ export async function postData(formData : FormData) {
                 }
             }
         }
-    })
+    });
+
+    const pusher = new Pusher({
+        appId: process.env.PUSHER_APP_ID,
+        key: process.env.NEXT_PUBLIC_PUSHER_KEY,
+        secret: process.env.PUSHER_SECRET,
+        cluster: "ap2",
+        useTLS: true,
+    });
+
+    pusher.trigger("chat", "hello", { message: `${JSON.stringify(data)}\n\n`});
 }
--- app/chat/[components]/chat.tsx
+++ app/chat/[components]/chat.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import { useState } from "react"
+import Image from "next/image";
+import Pusher from "pusher-js";
+import { useEffect, useRef, useState } from "react"
 
 interface iAppProps {
     data: {
@@ -12,14 +14,57 @@ interface iAppProps {
     }[];
 }
 
-export default function ChatComponent({data}: iAppProps) {
-    const [totalComments, setTotalComments] = useState();
+export default function ChatComponent({ data }: iAppProps) {
+    const [totalComments, setTotalComments] = useState(data);
+
+    const messageEndRef = useRef<HTMLInputElement>(null);
+
+    var pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
+        cluster: 'ap2'
+      });
+
+      var channel = pusher.subscribe('chat');
+    channel.bind('hello', function(data: any) {
+      const parsedComments = JSON.parse(data.message);
+
+      setTotalComments((prev)=> [...prev, parsedComments]);
+    });
+
+    const scrollToBottom = () => {
+        messageEndRef.current?.scrollIntoView({
+            behavior: "smooth"
+        })
+    }
+
+    useEffect(() => {
+        scrollToBottom();
+    },[totalComments]);
 
 
     return (
         <div className="p-6 flex-grow max-h-screen overflow-y-auto py-32">
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-5">
+                {totalComments.map((message, index) => (
+                    <div key={index} className="px-4 py-4 border border-gray-300 rounded-lg">
+                        <div className="flex items-center gap-10">
+                            <Image
+                                src={message.User.image as string} alt="profile image of user"
+                                width={50}
+                                height={50}
+                                className="w-12 h-1/2 object-cover rounded-full mr-4"
+                            />
 
+                            <div className="rounded-lg bg-white p-4 shadow-md self-start">
+                                {message.message}
+                            </div>
+
+                        </div>
+                        <p className="font-light text-xs text-gray-600">
+                            {message.User.name}
+                        </p>
+                    </div>
+                ))}
+                <div ref={messageEndRef}></div>
             </div>
         </div>
     )
